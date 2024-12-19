499dbaeec65e3c50efb970debee504caff241264
unknown <jalnan2000@naver.com>
2024-12-19 16:33:55 +0900

    12월 19일 python 데이터사이언스

---
+++ day4.py
@@ -0,0 +1,16 @@
+
+# arr = [[0 for _ in range(3)] for _ in range(3)]
+# num = int(input())
+
+# for i in range(3):
+#     for j in range(3):
+#         arr[i][j] = num
+#         print(arr[i][j], end = ' ')
+#         num += 1
+#     print()
+
+num = int(input())
+    
+if num == 3 or 5 or 7:
+    for i in range(10):
+        print(i+1, end = '')
